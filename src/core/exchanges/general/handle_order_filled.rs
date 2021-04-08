e3d082c3b474c787198be21909c5c67239d62a5b
Anton Sapunov <fort.sav.28@gmail.com>
2021-04-08 21:52:15 +0700

    take_roll_from_order_if_specified completed

--- src/core/exchanges/general/handle_order_filled.rs
+++ src/core/exchanges/general/handle_order_filled.rs
@@ -335,8 +335,6 @@ impl Exchange {
                         last_fill_amount,
                         last_fill_price,
                     );
-                dbg!(&last_fill_amount_in_currency_code);
-                dbg!(&commission_rate);
                 last_fill_amount_in_currency_code * commission_rate
             }
         }
@@ -413,7 +411,6 @@ impl Exchange {
             &commission_currency_code,
             &currency_pair_metadata,
         );
-        dbg!(&commission_amount);
 
         let mut converted_commission_currency_code = commission_currency_code.clone();
         let mut converted_commission_amount = commission_amount;
@@ -2214,12 +2211,73 @@ mod test {
                 assert_eq!(fills.len(), 1);
 
                 let fill = &fills[0];
-                dbg!(&fill);
                 let right_value = dec!(0.2) / dec!(100) * dec!(5);
                 assert_eq!(fill.commission_amount(), right_value);
             }
-            Err(error) => {
-                dbg!(&error);
+            Err(_) => assert!(false),
+        }
+    }
+
+    #[test]
+    fn take_roll_from_order_if_specified() {
+        let (exchange, _event_receiver) = get_test_exchange(false);
+
+        let client_order_id = ClientOrderId::unique_id();
+        let currency_pair = CurrencyPair::from_currency_codes("phb".into(), "btc".into());
+        let order_side = OrderSide::Buy;
+        let fill_amount = dec!(5);
+        let order_amount = dec!(12);
+        let trade_id = "test_trade_id".to_owned();
+
+        let mut event_data = FillEventData {
+            source_type: EventSourceType::WebSocket,
+            trade_id: trade_id.clone(),
+            client_order_id: None,
+            exchange_order_id: ExchangeOrderId::new("".into()),
+            fill_price: dec!(0.8),
+            fill_amount,
+            is_diff: true,
+            total_filled_amount: None,
+            order_role: None,
+            commission_currency_code: None,
+            commission_rate: None,
+            commission_amount: None,
+            fill_type: OrderFillType::Liquidation,
+            trade_currency_pair: Some(currency_pair.clone()),
+            order_side: Some(OrderSide::Buy),
+            order_amount: Some(dec!(0)),
+        };
+
+        let mut order = OrderSnapshot::with_params(
+            client_order_id.clone(),
+            OrderType::Liquidation,
+            Some(OrderRole::Maker),
+            exchange.exchange_account_id.clone(),
+            currency_pair,
+            dec!(0.2),
+            order_amount,
+            order_side,
+            None,
+        );
+        order.fills.filled_amount = dec!(3);
+
+        let order_pool = OrdersPool::new();
+        order_pool.add_snapshot_initial(Arc::new(RwLock::new(order)));
+        let order_ref = order_pool
+            .by_client_id
+            .get(&client_order_id)
+            .expect("in test");
+
+        match exchange.local_order_exist(&mut event_data, &*order_ref) {
+            Ok(_) => {
+                let (fills, _) = order_ref.get_fills();
+                assert_eq!(fills.len(), 1);
+
+                let fill = &fills[0];
+                let right_value = dec!(0.1) / dec!(100) * dec!(5);
+                assert_eq!(fill.commission_amount(), right_value);
+            }
+            Err(_) => {
                 assert!(false);
             }
         }
