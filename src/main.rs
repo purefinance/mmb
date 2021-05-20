55b3ca59c57421efdc4456247695b0194d38a7bb
Anton Sapunov <fort.sav.28@gmail.com>
2021-05-20 21:17:50 +0700

    Just to save

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{thread, time};
+
 use mmb_lib::core::lifecycle::launcher::{launch_trading_engine, EngineBuildConfig};
 
 #[allow(dead_code)]
@@ -5,4 +7,7 @@ use mmb_lib::core::lifecycle::launcher::{launch_trading_engine, EngineBuildConfi
 async fn main() {
     let engine_config = EngineBuildConfig::standard();
     launch_trading_engine::<()>(&engine_config).await;
+
+    println!("WAITING 10 SECONDS");
+    thread::sleep(time::Duration::from_secs(10));
 }
