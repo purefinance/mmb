fb7246d5f8d63bf2ed5e16e0c5eb28b32fede30f
Anton Sapunov <fort.sav.28@gmail.com>
2021-07-19 22:58:08 +0700

    Request metadata fixed

--- tests/binance/request_metadata.rs
+++ tests/binance/request_metadata.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 
-use chrono::Utc;
 use mmb::exchanges::{
     events::AllowedEventSourceType, general::commission::Commission,
     timeouts::timeout_manager::TimeoutManager,
@@ -11,13 +10,9 @@ use mmb_lib::core::exchanges::common::*;
 use mmb_lib::core::exchanges::general::exchange::*;
 use mmb_lib::core::exchanges::general::features::*;
 use mmb_lib::core::lifecycle::cancellation_token::CancellationToken;
-use mmb_lib::core::orders::event::OrderEventType;
-use mmb_lib::core::orders::order::*;
 use mmb_lib::core::settings;
-use rust_decimal_macros::*;
 
 use crate::get_binance_credentials_or_exit;
-use mmb_lib::core::exchanges::events::ExchangeEvent;
 use mmb_lib::core::exchanges::traits::ExchangeClientBuilder;
 use mmb_lib::core::lifecycle::application_manager::ApplicationManager;
 use tokio::sync::broadcast;
@@ -35,7 +30,7 @@ async fn request_metadata() {
     );
 
     let application_manager = ApplicationManager::new(CancellationToken::new());
-    let (tx, mut rx) = broadcast::channel(10);
+    let (tx, mut _rx) = broadcast::channel(10);
 
     BinanceBuilder.extend_settings(&mut settings);
 
@@ -65,66 +60,6 @@ async fn request_metadata() {
 
     exchange.clone().connect().await;
 
-    let test_order_client_id = ClientOrderId::unique_id();
-    let test_currency_pair = CurrencyPair::from_codes("phb".into(), "btc".into());
-    let order_header = OrderHeader::new(
-        test_order_client_id.clone(),
-        Utc::now(),
-        exchange_account_id.clone(),
-        test_currency_pair.clone(),
-        OrderType::Limit,
-        OrderSide::Buy,
-        dec!(10000),
-        OrderExecutionType::None,
-        None,
-        None,
-        "FromCreateOrderTest".to_owned(),
-    );
-
-    let order_to_create = OrderCreating {
-        header: order_header.clone(),
-        price: dec!(0.0000001),
-    };
-
-    // Should be called before any other api calls!
+    // If it's not panicked metadata fetched successfully
     exchange.build_metadata().await;
-    let _ = exchange
-        .cancel_all_orders(test_currency_pair.clone())
-        .await
-        .expect("in test");
-
-    //match created_order {
-    //    Ok(order_ref) => {
-    //        let event = rx
-    //            .recv()
-    //            .await
-    //            .expect("CreateOrderSucceeded event had to be occurred");
-    //        let order_event = if let ExchangeEvent::OrderEvent(order_event) = event {
-    //            order_event
-    //        } else {
-    //            panic!("Should receive OrderEvent")
-    //        };
-
-    //        if let OrderEventType::CreateOrderSucceeded = order_event.event_type {
-    //        } else {
-    //            panic!("Should receive CreateOrderSucceeded event type")
-    //        }
-
-    //        let exchange_order_id = order_ref.exchange_order_id().expect("in test");
-    //        let order_to_cancel = OrderCancelling {
-    //            header: order_header,
-    //            exchange_order_id,
-    //        };
-
-    //        // Cancel last order
-    //        let _cancel_outcome = exchange
-    //            .cancel_order(&order_to_cancel, CancellationToken::default())
-    //            .await;
-    //    }
-
-    //    // Create order failed
-    //    Err(_) => {
-    //        assert!(false)
-    //    }
-    //}
 }
