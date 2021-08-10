02fc1efa72cbcc3d0af0033b8cae3e4944c0c480
Borovkov Sergey <707grays@gmail.com>
2021-08-10 17:48:37 +0700

    added DerivativePositionInfo and primary implementation of ActivePosition

---
+++ src/core/exchanges/get_active_position.rs
@@ -0,0 +1,29 @@
+use crate::core::exchanges::common::{Amount, CurrencyPair};
+use crate::core::misc::derivative_position_info::DerivativePositionInfo;
+use crate::core::orders::order::OrderSide;
+
+use rust_decimal::Decimal;
+
+struct ActivePosition {
+    // pub id: usize,
+    pub currency_pair: CurrencyPair,
+    pub order_side: OrderSide,
+    // pub base: Decimal,
+    pub amount: Amount,
+    pub leverage: Decimal,
+    pub average_entry_price: Decimal,
+    pub liquidation_price: Decimal,
+}
+
+impl ActivePosition {
+    pub fn new(positionInfo: DerivativePositionInfo) -> ActivePosition {
+        ActivePosition {
+            currency_pair: positionInfo.currency_pair,
+            order_side: positionInfo.side,
+            amount: positionInfo.position,
+            leverage: positionInfo.leverage,
+            average_entry_price: positionInfo.average_entry_price,
+            liquidation_price: positionInfo.liquidation_price,
+        }
+    }
+}
