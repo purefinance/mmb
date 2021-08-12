896d9af8ccca29017be97b66068017cbbc3d3087
Borovkov Sergey <707grays@gmail.com>
2021-08-12 13:00:57 +0700

    Added Balances

---
+++ src/core/balance_manager/balances.rs
@@ -0,0 +1,32 @@
+use std::collections::HashMap;
+
+use crate::core::balance_manager::{
+    balance_position_by_fill_amount::BalancePositionByFillAmount,
+    balance_reservation::BalanceReservation,
+};
+use crate::core::exchanges::common::TradePlaceAccount;
+use crate::core::misc::service_value_tree::ServiceValueTree;
+use crate::core::orders::fill::OrderFill;
+
+use rust_decimal::Decimal;
+pub(crate) struct Balances {
+    pub current_version: usize,
+    pub version: usize,
+    pub balances_by_exchange_id: Option<HashMap<String, HashMap<String, Decimal>>>,
+    pub virtual_diff_balances: Option<ServiceValueTree>,
+
+    /// In Amount currency
+    pub reserved_amount: Option<ServiceValueTree>,
+
+    /// In Amount currency
+    pub position_by_fill_amount: Option<BalancePositionByFillAmount>,
+
+    /// In Amount currency
+    pub amount_limits: Option<ServiceValueTree>,
+    pub balance_reservations_by_reservation_id: Option<HashMap<u64, BalanceReservation>>,
+
+    pub last_order_fills: HashMap<TradePlaceAccount, OrderFill>,
+
+    /// Just for serialization/deserialization
+    pub serialized_last_order_fills: Vec<(TradePlaceAccount, OrderFill)>,
+}
--- src/core/balance_manager/mod.rs
+++ src/core/balance_manager/mod.rs
@@ -2,4 +2,5 @@ mod approved_part;
 mod balance_manager;
 pub(crate) mod balance_position_by_fill_amount;
 pub(crate) mod balance_reservation;
+pub(crate) mod balances;
 mod position_change;
