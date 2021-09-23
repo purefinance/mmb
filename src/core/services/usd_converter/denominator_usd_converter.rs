27d2e796ceb9297267e65f06a6dcfab021515f87
Borovkov Sergey <707grays@gmail.com>
2021-09-23 15:41:42 +0700

    added DenominatorUsdConverter

---
+++ src/core/services/usd_converter/denominator_usd_converter.rs
@@ -0,0 +1,32 @@
+use std::sync::Arc;
+
+use futures::executor::block_on;
+use tokio::sync::Mutex;
+
+use crate::core::{
+    exchanges::common::{Amount, CurrencyCode},
+    lifecycle::cancellation_token::CancellationToken,
+};
+
+use super::usd_denominator::UsdDenominator;
+
+pub struct DenominatorUsdConverter {
+    usd_denominator: Arc<Mutex<UsdDenominator>>,
+}
+
+impl DenominatorUsdConverter {
+    pub fn new(usd_denominator: Arc<Mutex<UsdDenominator>>) -> Self {
+        Self { usd_denominator }
+    }
+
+    pub(super) async fn calculate_using_denominator(
+        &self,
+        from_currency_code: CurrencyCode,
+        src_amount: Amount,
+    ) -> Option<Amount> {
+        self.usd_denominator
+            .lock()
+            .await
+            .currency_to_usd(&from_currency_code, src_amount)
+    }
+}
--- src/core/services/usd_converter/mod.rs
+++ src/core/services/usd_converter/mod.rs
@@ -1,2 +1,3 @@
+pub mod denominator_usd_converter;
 pub mod usd_converter;
 pub mod usd_denominator;
