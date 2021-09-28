7610d4fa8d01eb0381fd02c47c33bd0785880d33
Borovkov Sergey <707grays@gmail.com>
2021-09-28 15:17:01 +0700

    added PriceSourcesSaver(without DataRecorder)

--- src/core/services/usd_converter/mod.rs
+++ src/core/services/usd_converter/mod.rs
@@ -2,6 +2,7 @@ pub mod denominator_usd_converter;
 pub mod price_source_service;
 pub(crate) mod prices_calculator;
 pub mod prices_source_chain;
+pub mod prices_source_saver;
 pub mod rebase_price_step;
 pub mod usd_converter;
 pub mod usd_denominator;
