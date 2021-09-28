8370d525291d4a98920aaffac2bc73ce46da0c68
Borovkov Sergey <707grays@gmail.com>
2021-09-28 14:14:49 +0700

    added prices_calculator

--- src/core/services/usd_converter/mod.rs
+++ src/core/services/usd_converter/mod.rs
@@ -1,5 +1,6 @@
 pub mod denominator_usd_converter;
 pub mod price_source_service;
+pub(crate) mod prices_calculator;
 pub mod prices_source_chain;
 pub mod rebase_price_step;
 pub mod usd_converter;
---
+++ src/core/services/usd_converter/prices_calculator.rs
@@ -0,0 +1,86 @@
+pub(crate) mod prices_calculator {
+    use std::collections::HashMap;
+
+    use rust_decimal_macros::dec;
+
+    use crate::core::{
+        exchanges::common::{Amount, Price, TradePlace},
+        misc::price_by_order_side::PriceByOrderSide,
+        order_book::local_snapshot_service::LocalSnapshotsService,
+        services::usd_converter::prices_source_chain::PriceSourceChain,
+        DateTime,
+    };
+
+    pub(crate) fn calculate(
+        src_amount: Amount,
+        prices_source_chain: &PriceSourceChain,
+        prices: HashMap<TradePlace, Price>,
+    ) -> Price {
+        calculate_amount_for_chain(src_amount, prices_source_chain, |trade_place| {
+            prices.get(trade_place).cloned()
+        })
+        .expect("Invalid price cache")
+    }
+
+    fn calculate_amount_for_chain<F>(
+        src_amount: Amount,
+        prices_source_chain: &PriceSourceChain,
+        calculate_price: F,
+    ) -> Option<Amount>
+    where
+        F: Fn(&TradePlace) -> Option<Price>,
+    {
+        let mut rebase_price = dec!(1);
+
+        for step in &prices_source_chain.rebase_price_steps {
+            let trade_place = TradePlace::new(
+                step.exchange_id.clone(),
+                step.currency_pair_metadata.currency_pair(),
+            );
+            let calculated_price = (calculate_price)(&trade_place)?;
+
+            match step.from_base_to_quote_currency {
+                true => rebase_price *= calculated_price,
+                false => rebase_price /= calculated_price,
+            }
+        }
+        Some(rebase_price * src_amount)
+    }
+
+    pub(crate) fn convert_amount_now(
+        src_amount: Amount,
+        local_snapshot_service: &LocalSnapshotsService,
+        prices_source_chain: &PriceSourceChain,
+    ) -> Option<Amount> {
+        calculate_amount_for_chain(src_amount, prices_source_chain, |trade_place| {
+            local_snapshot_service.calculate_middle_price(trade_place)
+        })
+    }
+
+    pub fn convert_amount_in_past(
+        src_amount: Amount,
+        price_cache: HashMap<TradePlace, PriceByOrderSide>,
+        time_in_past: DateTime,
+        prices_source_chain: &PriceSourceChain,
+    ) -> Option<Amount> {
+        calculate_amount_for_chain(src_amount, prices_source_chain, |trade_place| {
+            let prices = match price_cache.get(trade_place) {
+                Some(prices) => prices,
+                None => {
+                    log::error!("Can't get price {:?} on time {}", trade_place, time_in_past);
+                    return None;
+                }
+            };
+            let top_ask = match prices.top_ask {
+                Some(top_ask) => top_ask,
+                None => return None,
+            };
+
+            let top_bid = match prices.top_bid {
+                Some(top_bid) => top_bid,
+                None => return None,
+            };
+            Some((top_ask + top_bid) / dec!(2))
+        })
+    }
+}
