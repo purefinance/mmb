ba642a9eedcc2a58a1ffc837539d73ac44f87737
Evgeny Khudoba <evgeny.khudoba@yandex.ru>
2021-10-23 22:39:05 +0700

    Removed unused field Exchange::supported_symbols + some refactoring in Exchange::build_metadata()

--- src/core/exchanges/general/exchange_creation.rs
+++ src/core/exchanges/general/exchange_creation.rs
@@ -1,14 +1,12 @@
 use std::sync::Arc;
 
-use itertools::Itertools;
-use log::error;
 use tokio::sync::broadcast;
 
-use super::{commission::Commission, currency_pair_metadata::CurrencyPairMetadata};
+use super::commission::Commission;
 use crate::core::exchanges::events::ExchangeEvent;
 use crate::core::lifecycle::application_manager::ApplicationManager;
 use crate::core::lifecycle::launcher::EngineBuildConfig;
-use crate::core::settings::{CurrencyPairSetting, ExchangeSettings};
+use crate::core::settings::ExchangeSettings;
 use crate::core::{
     exchanges::{
         general::exchange::Exchange,
@@ -70,60 +68,9 @@ pub async fn create_exchange(
         Commission::default(),
     );
 
-    exchange.build_metadata().await;
-
-    if let Some(currency_pairs) = &user_settings.currency_pairs {
-        exchange.set_symbols(get_symbols(&exchange, &currency_pairs[..]))
-    }
+    exchange.build_metadata(&user_settings.currency_pairs).await;
 
     exchange.clone().connect().await;
 
     exchange
-}
-
-pub fn get_symbols(
-    exchange: &Arc<Exchange>,
-    currency_pairs: &[CurrencyPairSetting],
-) -> Vec<Arc<CurrencyPairMetadata>> {
-    let mut symbols = Vec::new();
-
-    let supported_symbols_guard = exchange.supported_symbols.lock();
-    for currency_pair_setting in currency_pairs {
-        let mut filtered_symbols = supported_symbols_guard
-            .iter()
-            .filter(|x| {
-                if let Some(currency_pair) = &currency_pair_setting.currency_pair {
-                    return currency_pair.as_str() == x.currency_pair().as_str();
-                }
-
-                return x.base_currency_code == currency_pair_setting.base
-                    && x.quote_currency_code == currency_pair_setting.quote;
-            })
-            .take(2)
-            .collect_vec();
-
-        let symbol = match filtered_symbols.len() {
-            0 => {
-                error!(
-                    "Unsupported symbol {:?} on exchange {}",
-                    currency_pair_setting, exchange.exchange_account_id
-                );
-                continue;
-            }
-            1 => filtered_symbols
-                .pop()
-                .expect("we checked already that 1 symbol found"),
-            _ => {
-                error!(
-                    "Found more then 1 symbol for currency pair {:?}. Found symbols: {:?}",
-                    currency_pair_setting, filtered_symbols
-                );
-                continue;
-            }
-        };
-
-        symbols.push(symbol.clone());
-    }
-
-    symbols
 }
--- src/core/exchanges/general/exchange_metadata.rs
+++ src/core/exchanges/general/exchange_metadata.rs
@@ -5,27 +5,54 @@ use log::warn;
 use rust_decimal_macros::dec;
 use std::sync::Arc;
 
-use crate::core::exchanges::common::{CurrencyCode, CurrencyId};
+use crate::core::exchanges::common::{CurrencyCode, CurrencyId, ExchangeAccountId};
+use crate::core::settings::CurrencyPairSetting;
 
 use super::{currency_pair_metadata::CurrencyPairMetadata, exchange::Exchange};
 
 impl Exchange {
-    pub async fn build_metadata(&self) {
-        let symbols;
+    pub async fn build_metadata(&self, currency_pair_settings: &Option<Vec<CurrencyPairSetting>>) {
+        let exchange_symbols = &self.request_metadata_with_retries().await;
 
+        let supported_currencies = get_supported_currencies(exchange_symbols);
+        self.setup_supported_currencies(supported_currencies);
+
+        let currency_pairs = exchange_symbols
+            .iter()
+            .map(|x| x.currency_pair())
+            .collect_vec();
+
+        for currency_pair in currency_pairs {
+            self.leverage_by_currency_pair
+                .insert(currency_pair, dec!(1));
+        }
+
+        if let Some(currency_pairs) = currency_pair_settings {
+            self.setup_symbols(get_symbols(
+                currency_pairs,
+                exchange_symbols,
+                self.exchange_account_id,
+            ));
+        } else {
+            panic!(
+                "Settings `currency_pairs` should be specified for exchange {}",
+                self.exchange_account_id
+            );
+        }
+    }
+
+    async fn request_metadata_with_retries(&self) -> Vec<Arc<CurrencyPairMetadata>> {
         const MAX_RETRIES: u8 = 5;
-        let mut retry = 0u8;
+        let mut retry = 0;
         loop {
             match self.build_metadata_core().await {
-                Ok(result_symbols) => {
-                    symbols = result_symbols;
-                    break;
-                }
+                Ok(result_symbols) => return result_symbols,
                 Err(error) => {
                     let error_message = format!(
                         "Unable to get metadata for {}: {:?}",
                         self.exchange_account_id, error
                     );
+
                     if retry < MAX_RETRIES {
                         warn!("{}", error_message);
                     } else {
@@ -35,24 +62,6 @@ impl Exchange {
             }
 
             retry += 1;
-        }
-
-        let supported_currencies = Self::get_supported_currencies(&symbols[..]);
-        self.set_supported_currencies(supported_currencies);
-
-        let currency_pairs = symbols.iter().map(|x| x.currency_pair()).collect_vec();
-        for currency_pair in currency_pairs {
-            self.leverage_by_currency_pair
-                .insert(currency_pair, dec!(1));
-        }
-        *self.supported_symbols.lock() = symbols;
-    }
-
-    fn set_supported_currencies(&self, supported_currencies: DashMap<CurrencyCode, CurrencyId>) {
-        for (currency_code, currency_id) in supported_currencies {
-            self.exchange_client
-                .get_supported_currencies()
-                .insert(currency_id, currency_code);
         }
     }
 
@@ -72,21 +81,15 @@ impl Exchange {
         }
     }
 
-    fn get_supported_currencies(
-        symbols: &[Arc<CurrencyPairMetadata>],
-    ) -> DashMap<CurrencyCode, CurrencyId> {
-        symbols
-            .iter()
-            .flat_map(|s| {
-                vec![
-                    (s.base_currency_code, s.base_currency_id),
-                    (s.quote_currency_code, s.quote_currency_id),
-                ]
-            })
-            .collect()
+    fn setup_supported_currencies(&self, supported_currencies: DashMap<CurrencyCode, CurrencyId>) {
+        for (currency_code, currency_id) in supported_currencies {
+            self.exchange_client
+                .get_supported_currencies()
+                .insert(currency_id, currency_code);
+        }
     }
 
-    pub fn set_symbols(&self, symbols: Vec<Arc<CurrencyPairMetadata>>) {
+    fn setup_symbols(&self, symbols: Vec<Arc<CurrencyPairMetadata>>) {
         let mut currencies = symbols
             .iter()
             .flat_map(|x| vec![x.base_currency_code, x.quote_currency_code])
@@ -94,15 +97,82 @@ impl Exchange {
         currencies.dedup();
         *self.currencies.lock() = currencies;
 
-        let mut current_specific_currencies = Vec::new();
         symbols.iter().for_each(|symbol| {
             self.symbols.insert(symbol.currency_pair(), symbol.clone());
-            current_specific_currencies.push(
-                self.exchange_client
-                    .get_specific_currency_pair(symbol.currency_pair()),
-            );
         });
+
+        let exchange_client = &self.exchange_client;
+        let current_specific_currencies = symbols
+            .iter()
+            .map(|x| exchange_client.get_specific_currency_pair(x.currency_pair()))
+            .collect_vec();
+
         self.exchange_client
             .set_traded_specific_currencies(current_specific_currencies);
     }
+}
+
+fn get_supported_currencies(
+    symbols: &[Arc<CurrencyPairMetadata>],
+) -> DashMap<CurrencyCode, CurrencyId> {
+    symbols
+        .iter()
+        .flat_map(|s| {
+            [
+                (s.base_currency_code, s.base_currency_id),
+                (s.quote_currency_code, s.quote_currency_id),
+            ]
+        })
+        .collect()
+}
+
+fn get_symbols(
+    currency_pairs: &[CurrencyPairSetting],
+    exchange_symbols: &[Arc<CurrencyPairMetadata>],
+    exchange_account_id: ExchangeAccountId,
+) -> Vec<Arc<CurrencyPairMetadata>> {
+    currency_pairs
+        .iter()
+        .filter_map(|x| get_matched_currency_pair(x, exchange_symbols, exchange_account_id))
+        .collect()
+}
+
+fn get_matched_currency_pair(
+    currency_pair_setting: &CurrencyPairSetting,
+    exchange_symbols: &[Arc<CurrencyPairMetadata>],
+    exchange_account_id: ExchangeAccountId,
+) -> Option<Arc<CurrencyPairMetadata>> {
+    // currency pair metadata and currency pairs from settings should match 1 to 1
+    let settings_currency_pair = currency_pair_setting.currency_pair.as_deref();
+    let filtered_metadata = exchange_symbols
+        .iter()
+        .filter(|metadata| {
+            return Some(metadata.currency_pair().as_str()) == settings_currency_pair
+                || metadata.base_currency_code == currency_pair_setting.base
+                    && metadata.quote_currency_code == currency_pair_setting.quote;
+        })
+        .take(2)
+        .cloned()
+        .collect_vec();
+
+    match filtered_metadata.as_slice() {
+        [] => {
+            log::error!(
+                "Unsupported symbol {:?} on exchange {}",
+                currency_pair_setting,
+                exchange_account_id
+            );
+        }
+        [metadata] => return Some(metadata.clone()),
+        _ => {
+            log::error!(
+                    "Found more then 1 symbol for currency pair {:?} on exchange {}. Found symbols: {:?}",
+                    currency_pair_setting,
+                    exchange_account_id,
+                    filtered_metadata
+                );
+        }
+    };
+
+    None
 }
