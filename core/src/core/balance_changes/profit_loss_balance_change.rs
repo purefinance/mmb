575e2a932b20e04da6f2bb7a8ca65fecbe70361a
Borovkov Sergey <707grays@gmail.com>
2021-12-27 13:38:22 +0700

    fixed tests

--- core/src/core/balance_changes/profit_loss_balance_change.rs
+++ core/src/core/balance_changes/profit_loss_balance_change.rs
@@ -18,44 +18,44 @@ use crate::core::{
 impl_u64_id!(ProfitLossBalanceChangeId);
 
 #[derive(Clone, Debug)]
-#[cfg_attr(test, allow(dead_code))]
+#[allow(dead_code)]
 pub(crate) struct ProfitLossBalanceChange {
-    pub _id: ProfitLossBalanceChangeId,
+    pub id: ProfitLossBalanceChangeId,
     pub client_order_fill_id: ClientOrderFillId,
     pub change_date: DateTime,
-    pub _service_name: ServiceName,
-    pub _service_configuration_key: ServiceConfigurationKey,
-    pub _exchange_id: ExchangeId,
+    pub service_name: ServiceName,
+    pub service_configuration_key: ServiceConfigurationKey,
+    pub exchange_id: ExchangeId,
     pub trade_place: TradePlaceAccount,
     pub currency_code: CurrencyCode,
     pub balance_change: Amount,
-    pub _usd_price: Price,
+    pub usd_price: Price,
     pub usd_balance_change: Amount,
 }
 
 impl ProfitLossBalanceChange {
     pub fn new(
         request: BalanceRequest,
-        _exchange_id: ExchangeId,
+        exchange_id: ExchangeId,
         client_order_fill_id: ClientOrderFillId,
         change_date: DateTime,
         balance_change: Amount,
         usd_balance_change: Amount,
     ) -> Self {
         Self {
-            _id: ProfitLossBalanceChangeId::generate(),
+            id: ProfitLossBalanceChangeId::generate(),
             client_order_fill_id,
             change_date,
-            _service_name: request.configuration_descriptor.service_name,
-            _service_configuration_key: request
+            service_name: request.configuration_descriptor.service_name,
+            service_configuration_key: request
                 .configuration_descriptor
                 .service_configuration_key
                 .clone(),
-            _exchange_id,
+            exchange_id,
             trade_place: TradePlaceAccount::new(request.exchange_account_id, request.currency_pair),
             currency_code: request.currency_code,
             balance_change,
-            _usd_price: usd_balance_change / balance_change,
+            usd_price: usd_balance_change / balance_change,
             usd_balance_change: usd_balance_change,
         }
     }
--- core/src/core/exchanges/timeouts/pre_reserved_group.rs
+++ core/src/core/exchanges/timeouts/pre_reserved_group.rs
@@ -1,21 +1,18 @@
 use crate::core::exchanges::timeouts::requests_timeout_manager::RequestGroupId;
 
 #[derive(Clone)]
+#[allow(dead_code)]
 pub struct PreReservedGroup {
     pub(crate) id: RequestGroupId,
-    pub(crate) _group_type: String,
+    pub(crate) group_type: String,
     pub(crate) pre_reserved_requests_count: usize,
 }
 
 impl PreReservedGroup {
-    pub fn new(
-        id: RequestGroupId,
-        _group_type: String,
-        pre_reserved_requests_count: usize,
-    ) -> Self {
+    pub fn new(id: RequestGroupId, group_type: String, pre_reserved_requests_count: usize) -> Self {
         Self {
             id,
-            _group_type,
+            group_type,
             pre_reserved_requests_count,
         }
     }
