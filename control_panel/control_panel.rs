52bda9fe787ddefda81b6452792cb2afa6984cfd
Borovkov Sergey <707grays@gmail.com>
2022-01-12 19:43:17 +0700

    review fixes

--- control_panel/control_panel.rs
+++ control_panel/control_panel.rs
@@ -68,6 +68,9 @@ impl ControlPanel {
         let client = self.client.clone();
 
         let server = HttpServer::new(move || {
+            let mut webui_dir = std::env::current_dir().expect("Unable get current directory");
+            webui_dir.push(r"webui");
+
             App::new()
                 .app_data(Data::new(client.clone()))
                 .service(endpoints::health)
@@ -76,7 +79,7 @@ impl ControlPanel {
                 .service(endpoints::get_config)
                 .service(endpoints::set_config)
                 .service(
-                    actix_files::Files::new("/", "./webui/")
+                    actix_files::Files::new("/", webui_dir)
                         .use_last_modified(true)
                         .index_file("index.html"),
                 )
@@ -174,7 +177,7 @@ pub async fn send_request(
         try_reconnect(client.clone(), try_counter).await;
 
         if try_counter > 2 {
-            return HttpResponse::RequestTimeout().body("Request Timeout");
+            return HttpResponse::ServiceUnavailable().body("Trading engine service unavailable");
         }
 
         tokio::time::sleep(Duration::from_secs(3)).await;
