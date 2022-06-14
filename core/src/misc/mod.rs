04a4d56c439ac1fae6db7f60967e0ccaec81c97a
Michael Watson <watson.micha@yandex.ru>
2022-06-14 06:00:09 +0000

    Added implementation of trade events for serum

--- core/src/misc/mod.rs
+++ core/src/misc/mod.rs
@@ -4,5 +4,5 @@ pub mod price_by_order_side;
 pub(crate) mod price_source_model;
 pub mod reserve_parameters;
 pub(crate) mod service_value_tree;
-pub(crate) mod time;
+pub mod time;
 pub mod traits;
