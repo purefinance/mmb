552a6cb2cf9f531ddfcceb9fa8d5e803f87a7816
Michael Watson <watson.micha@yandex.ru>
2022-06-20 14:00:10 +0000

    Implemented function for applying migrations

---
+++ mmb_database/src/postgres_db/sql/first_test_migrations/migrations/20220609121700_test1.up.sql
@@ -0,0 +1,4 @@
+CREATE TABLE test_mig1(
+    id bigserial PRIMARY KEY,
+    insert_time timestamp WITH TIME ZONE NOT NULL
+);
---
+++ mmb_database/src/postgres_db/sql/second_migrations/migrations/20220609122711_test1_setp2.up.sql
@@ -0,0 +1,4 @@
+CREATE TABLE test_mig2(
+    id bigserial PRIMARY KEY,
+    insert_time timestamp WITH TIME ZONE NOT NULL
+);
